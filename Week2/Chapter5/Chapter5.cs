6eec8e7786e9fe318324741cb08c4586db84e898
Brandon Gunthner <Brandon.Gunthner@vu.com>
2021-09-17 16:43:53 -0500

    Created Solution and code files

---
+++ Week2/Chapter5/Chapter5.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chapter5
+{
+    class Chapter5
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Week2/Week2/Chapter3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chapter3
+{
+    class Chapter3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
