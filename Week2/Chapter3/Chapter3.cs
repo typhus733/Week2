18b3c1beef21299ed96ebb473c2c9946360754b6
Brandon Gunthner <typhus733@gmail.com>
2021-09-19 21:12:22 -0500

    Completed Chapter 3

--- Week2/Chapter3/Chapter3.cs
+++ Week2/Chapter3/Chapter3.cs
@@ -6,7 +6,34 @@ namespace Chapter3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //#3
+            int num;
+            Console.WriteLine("Enter a number");
+            num = Int32.Parse(Console.ReadLine());
+
+            num /= 100;
+            num %= 10;
+            if (num == 7)
+             {
+
+                Console.WriteLine("The third digit is 7!");
+
+            }
+            else
+            {
+                Console.WriteLine("The third digit is not 7...");
+            }
+
+            //#7
+            double weight;
+
+            Console.WriteLine("Enter a weight in lbs");
+            weight = double.Parse(Console.ReadLine());
+
+            weight *= 0.17;
+            Console.WriteLine("The weight on the moon would be {0} lbs", weight);
+
+
         }
     }
 }
